23356728fc873e94a734260239760fd94b82c1c6
sondos-braim <sondos9811@gmail.com>
2021-01-06 21:24:27 +0200

    lab 29

---
+++ django_custom_user/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class CustomUser(AbstractUser):
+    first_name=models.CharField(max_length=64)
+    last_name=models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.first_name
